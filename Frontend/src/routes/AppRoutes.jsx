42ad1e5fc5b0d1a12c11bfa5796c6a8100d8b6b1
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-04-19 23:42:56 +0530

    user-context-setup

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route,BrowserRouter,Routes } from 'react-router-dom'
 import Login from '../screens/Login';
 import Register from '../screens/Register';
+import Home from '../screens/Home';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>}></Route>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
       </Routes>
--- Frontend/src/screens/Login.jsx
+++ Frontend/src/screens/Login.jsx
@@ -1,32 +1,34 @@
-import React, { useState,  } from "react";
+import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "../config/axios";
-
+import { UserContext } from "../context/user.context";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const { setUser } = useContext(UserContext);
 
   const navigate = useNavigate();
 
-function submitHandler(e) {
-  e.preventDefault();
+  function submitHandler(e) {
+    e.preventDefault();
 
-  axios
-    .post("/users/login", {
-      email,
-      password,
-    })
-    .then((res) => {
-      console.log(res.data);
-      navigate("/");
-    })
-    .catch((err) => {
-      console.error("Login error:", err); // Log the entire error object
-      console.log(err.response?.data || "An unexpected error occurred."); // Safely access err.response.data
-    });
-}
+    axios
+      .post("/users/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("token", res.data.token);
+        setUser(res.data.user);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.error("Login error:", err); // Log the entire error object
+        console.log(err.response?.data || "An unexpected error occurred."); // Safely access err.response.data
+      });
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
