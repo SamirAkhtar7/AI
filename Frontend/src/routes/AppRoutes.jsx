292853e54ae48efd524a833b11209af05e0a9903
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-04-19 15:15:06 +0530

    login&register complete

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route,BrowserRouter,Routes } from 'react-router-dom'
+import Login from '../screens/Login';
+import Register from '../screens/Register';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Home</div>}></Route>
-        <Route path="/login" element={<div>login</div>}></Route>
-        <Route path="/register" element={<div>register</div>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
       </Routes>
     </BrowserRouter>
   );
